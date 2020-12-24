793fbba6ca8e6106dd33d89d3d6788a93cd4800e
Wouter Gritter <woutergritter@gmail.com>
2020-12-24 16:13:00 +0100

    Remove unneeded utility classes

--- src/main/java/me/woutergritter/wenchantments/util/function/ThrowingConsumer.java
+++
@@ -1,6 +0,0 @@
-package me.woutergritter.wenchantments.util.function;
-
-@FunctionalInterface
-public interface ThrowingConsumer<T, E extends Throwable> {
-    void accept(T t) throws E;
-}
--- src/main/java/me/woutergritter/wenchantments/util/function/ThrowingSupplier.java
+++
@@ -1,6 +0,0 @@
-package me.woutergritter.wenchantments.util.function;
-
-@FunctionalInterface
-public interface ThrowingSupplier<T, E extends Throwable> {
-    T get() throws E;
-}
